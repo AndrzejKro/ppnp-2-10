2f76f2deec1059fcde6081cd6653816410b2004a
Andrzej <andrzejkroczek@gmail.com>
2023-10-06 15:03:41 +0200

    Wykresy

--- Wykres 2.py
+++ Wykres 2.py
@@ -10,7 +10,9 @@ plt.title("Histogram")
 plt.ylabel("Częstość")
 plt.xlabel("Wartość")
 plt.savefig("Wykres.png") # zapisanie pliku png
-plt.savefig("Wykres.pdf")
+plt.savefig("Wykres.pdf") # zapisanie pliku pdf
 
 plt.show()
+
+
 
